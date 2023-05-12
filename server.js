0a5e45cdc3b97d975c0cf34c5f2d66fcd316e8f5
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-05-12 00:10:45 -0300

    fixed cors error

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 8000
+
+app.use(cors())
 
 app.listen(process.env.PORT || PORT, () => {
     console.log('Server running on port 8000')
